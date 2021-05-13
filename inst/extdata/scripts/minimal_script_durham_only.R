f17d09a931d8fbd7c903186e1e475255d60eb99e
Kylie Ainslie <kylieainslie@Kylies-MBP.attlocal.net>
2021-05-13 17:28:39 -0400

    run simulations with rotavirus manuscript input file and save output files

--- inst/extdata/scripts/minimal_script_durham_only.R
+++ inst/extdata/scripts/minimal_script_durham_only.R
@@ -22,7 +22,8 @@ devtools::load_all()
 ### Read parameters from input files
 #   you can specify the folder and file names of the input file within the ""
 #   if no path is specified a window will pop up and allow you to choose a file from your computer
-params <- readParams("./inst/extdata/input/SimVEE_MI_RCT_06_04_00_input.csv")
+# params <- readParams("./inst/extdata/input/SimVEE_MI_RCT_06_04_00_input.csv")
+params <- readParams("./inst/extdata/input/Rotavirus_Input_ban_406.csv")
 
 ### run simulation
 #   there is an optional path argument for run_simvee(params, path = )
