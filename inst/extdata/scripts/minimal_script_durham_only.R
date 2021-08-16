77e782294dd70d027a5224eafae0b0b499ad1d1b
Kylie Ainslie <kylieainslie@Kylies-MacBook-Pro.local>
2021-08-16 14:56:33 +0200

    update minimal script to exclude simulations that don't have an event in the final period

--- inst/extdata/scripts/minimal_script_durham_only.R
+++ inst/extdata/scripts/minimal_script_durham_only.R
@@ -1,6 +1,8 @@
-# SIMVEE minimal script
-# update: 15 April, 2021
+# SIMVEE minimal script --------------------------------------------------------
+# update: 16 August, 2021
 # time: 12:27
+
+# install required packages ----------------------------------------------------
 # install.packages("timereg")
 # install.packages("DEoptim")
 library(dplyr)
@@ -12,34 +14,35 @@ library(ggplot2)
 # library(DEoptim)
 library(readr)
 
-### Source functions from other files
+### Source functions from other files ------------------------------------------
 # source('R/simvee.R')
 # source('R/readParams.R')
 # source('R/ve_methods.R')
 
-### Load wave package
-# makse sure you're in the wave root directory!
+### Load wave package ----------------------------------------------------------
+# make sure you're in the wave root directory!
 devtools::load_all()
-### Read parameters from input files
+### Read parameters from input files -------------------------------------------
 #   you can specify the folder and file names of the input file within the ""
-#   if no path is specified a window will pop up and allow you to choose a file from your computer
-params <- readParams("./inst/extdata/input/SimVEE_MI_RCT_06_04_03_input.csv")
-# params <- readParams("inst/extdata/input/Input_ban_400.csv")
+#   if no path is specified a window will pop up and allow you to choose a file
+#   from your computer
+params <- readParams("./inst/extdata/input/SimVEE_MI_RCT_06_04_00_input.csv")
 
-### run simulation
+### run simulation -------------------------------------------------------------
 #   there is an optional path argument for run_simvee(params, path = )
 #   if no path is specified, it will default to current working directory
-outcomes_dat <- run_simvee(params)
+# outcomes_dat <- run_simvee(params)
 
-### read in outcomes file
+### read in outcomes file ------------------------------------------------------
 # you can specify the file name/path of the output file inside ""
-# outcomes_dat <- read.csv(file.choose())
+file_path <- "inst/extdata/output/Outcomes_MI_RCT_06_04_00.csv"
+outcomes_dat <- read_csv(file_path)
 
 # add FARI indicator variable
 outcomes_dat1 <- outcomes_dat %>% mutate(FARI = ifelse(DINF == 0, 0, 1),
                                          DINF_new = ifelse(DINF == 0, 999, DINF))
 
-# only estimate VE using Durham method
+### estimate VE using Durham method --------------------------------------------
 # this is so we can troubleshoot this method
 
 reject_h0_durham <- 0 # set h0 rejection counter to 0
@@ -54,30 +57,61 @@ for (i in 1:params$sim){
   # fit ordinary Cox propotional hazards model
   flu_coxmod <- coxph(Surv(DINF_new,FARI) ~ V, data = dat)
 
-  # test the proportional hazards assumption and compute the Schoenfeld residuals ($y)
+  # test the proportional hazards assumption and compute the Schoenfeld
+  # residuals ($y)
   flu_zph <- cox.zph(fit = flu_coxmod, transform = "identity")
   reject_h0_durham <- reject_h0_durham + ifelse(flu_zph$table[1,3] < 0.05, 1, 0)
 
   # calculate VE
-  rtn[[i]] <- durham_ve(flu_zph, df = 4, n_days = params$ND, n_periods = params$NJ,
-                    n_days_period = params$NDJ, var = "V") %>%
-    mutate(Sim = i, Method = "Durham")
+  end_point <- params$ND - (params$NDJ/2)
+  if (max(flu_zph$x) <= end_point){
+    rtn[[i]] <- NA
+  } else {
+    rtn[[i]] <- durham_ve(flu_zph, df = 4, n_days = params$ND,
+                          n_periods = params$NJ, n_days_period = params$NDJ,
+                          var = "V") %>%
+      mutate(Sim = i, Method = "Durham")
+  }
 }
+# ------------------------------------------------------------------------------
 
+# results ----------------------------------------------------------------------
+
+# what proportion of the time do we reject H0?
+prop_reject <- reject_h0_durham/params$sim
+prop_reject
 # bind the outputs of each simulation
-results <- bind_rows(rtn) %>%
+find_nas <- is.na(rtn)
+rtn1 <- rtn[!find_nas]
+
+results <- bind_rows(rtn1)
+
+# save output
+saveRDS(results, file = paste0("inst/extdata/output/sim_results_", params$title, ".rds"))
+
+# plot all sims
+p <- ggplot(data = results, aes(x = period, y = V, color = as.factor(Sim))) + #
+  geom_line() +
+  #geom_ribbon(aes(ymin=lower, ymax=upper), alpha = 0.1, linetype = "dashed") + # , fill = waning_rate
+  #facet_wrap(~waning_rate, nrow = 3) +
+  theme(legend.position = "none")
+p
+
+# summarize results
+summary_results <- results %>%
   select(Sim, period, V) %>%
   group_by(period) %>%
-  summarise_at(.vars = "V", .funs = c("mean", "sd")) %>%
+  summarise_at(.vars = "V", .funs = c("mean", "median", "sd")) %>%
   mutate(lower = mean - 1.96 * sd,
          upper = mean + 1.96 * sd,
-         lower_new = ifelse(lower < -0.5, -0.5, lower),
-         upper_new = ifelse(upper > 1.5, 1.5, upper))
+         # lower_new = ifelse(lower < -0.5, -0.5, lower),
+         # upper_new = ifelse(upper > 1.5, 1.5, upper)
+         )
 
-# plot
-p <- ggplot(data = results, aes(x = period, y = mean)) + # , color = waning_rate
+# plot summarized results
+p <- ggplot(data = summary_results, aes(x = period, y = mean)) + # , color = waning_rate
   geom_line() +
-  geom_ribbon(aes(ymin=lower_new, ymax=upper_new), alpha = 0.1, linetype = "dashed") + # , fill = waning_rate
+  geom_ribbon(aes(ymin=lower, ymax=upper), alpha = 0.1, linetype = "dashed") + # , fill = waning_rate
   #facet_wrap(~waning_rate, nrow = 3) +
   theme_bw()
 p
