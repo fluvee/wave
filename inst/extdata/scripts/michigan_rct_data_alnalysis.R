ea6dbd92264bffbebf8f0e4c524021da01103c16
Kylie Ainslie <kylieainslie@Kylies-MacBook-Pro.local>
2021-08-19 17:53:43 +0200

    plot results from durham method

--- inst/extdata/scripts/michigan_rct_data_alnalysis.R
+++ inst/extdata/scripts/michigan_rct_data_alnalysis.R
@@ -43,7 +43,7 @@ mi_data_laiv <- mi_data1 %>%
 # fit ordinary Cox propotional hazards model (just for IIV first)
 flu_coxmod <- coxph(Surv(DINF_new,influenza) ~ V, data = mi_data_iiv)
 
-# test the proportional hazards assumption and compute the Schoenfeld residuals ($y)
+# test the proportional hazards assumption and compute the Schoenfeld residuals
 flu_zph <- cox.zph(fit = flu_coxmod, transform = "identity")
 # hypothesis test of whether beta varies over time
 flu_zph$table[1,3]
@@ -54,13 +54,13 @@ abs_ve <- 1 - exp(flu_coxmod$coefficients[1])
 # VE(t)
 time_points <- flu_zph$x # time points
 schoenfeld <- flu_zph$y # Schoenfeld residuals
+
 # fit loess to Schoenfeld residuals + beta
 # this approach is from Petrie et al. 2016 (JID)
 for_loess <- data.frame(x = time_points,
                         V = schoenfeld[,1] + flu_coxmod$coefficients[1])
 
 l <- loess(V~x, data = for_loess, degree = 1)
-f <- function(x) predict(l, newdata = x)
 ve_t <- 1 - exp(l$fitted) # VE(t)
 # get confidence intervals
 pred <- predict(l, for_loess, se = TRUE)
@@ -68,10 +68,52 @@ ve_t_lower <- 1- exp(pred$fit - 1.96*pred$se.fit)
 ve_t_upper <- 1 - exp(pred$fit + 1.96*pred$se.fit)
 # integrate loess function and divide by days between first and last case
 # of symptomatic influenza to get average VE(t)
+f <- function(x) predict(l, newdata = x)
 int <- integrate(f, min(time_points), max(time_points))
 ve_t_avg <- 1 - exp(int$value / (max(time_points) - min(time_points)))
 # ---------------------------------------------------------------------------------------
 
+# ---------------------------------------------------------------------------------------
+# output (save for each vaccine)
+# IIV
+first_date <- mi_data_iiv[which(mi_data_iiv$DINF_new == min(time_points)),]$onset_date
+pred_dates <- first_date + (time_points - min(time_points))
+ve_dat_iiv <- tibble(time_point = time_points,
+                     date = pred_dates,
+                     ve = ve_t,
+                     lower = ve_t_upper,
+                     upper = ve_t_lower,
+                     vac_type = "IIV")
+
+# LAIV
+first_date <- mi_data_laiv[which(mi_data_laiv$DINF_new == min(time_points)),]$onset_date
+pred_dates <- first_date + (time_points - min(time_points))
+ve_dat_laiv <- tibble(time_point = time_points,
+                      date = pred_dates,
+                      ve = ve_t,
+                      lower = ve_t_upper,
+                      upper = ve_t_lower,
+                      vac_type = "LAIV")
+# for output
+ve_output <- bind_rows(ve_dat_iiv, ve_dat_laiv)
+write_csv(ve_output, file = "VE_est_durham.csv")
+
+# combine data sets for plotting and output
+ve_dat_plot <- ve_output %>%
+  filter(vac_type == "IIV")
+  #mutate(lower = ifelse(lower < -0.25, -0.25, lower))
+
+p_petrie <- ggplot(data = ve_dat_plot, aes(x = date, y = ve)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha=0.2) +
+  labs(y = "VE(t)", x = "Date", title = "Loess") +
+  scale_y_continuous(limits = c(-5, 1)) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  theme_bw() #+
+  #facet_grid(. ~ vac_type)
+p_petrie
+
+# ---------------------------------------------------------------------------------------
 # calculate VE and CIs using bootstrap --------------------------------------------------
 library(boot)
 # function to obtain VE(t) from the data
@@ -81,12 +123,12 @@ get_ve_t <- function(n, data, indices) {
   zph <- cox.zph(fit = coxmod, transform = "identity") # test the proportional hazards assumption and compute the Schoenfeld residuals ($y)
   xx <- zph$x # time points
   yy <- zph$y # Schoenfeld residuals
-  d <- nrow(yy)
+  #d <- nrow(yy)
   nvar <- ncol(yy)
   pred.x <- seq(from = min(xx), to = max(xx), length = n)
     #seq(from = (n_days_period/2), to = n_days - (n_days_period/2), length = n_periods)
   temp <- c(pred.x, xx)
-  lmat <- ns(temp, df = 4, intercept = TRUE)
+  lmat <- ns(temp, df = 2, intercept = TRUE)
   pmat <- lmat[1:length(pred.x), ]
   xmat <- lmat[-(1:length(pred.x)), ]
   qmat <- qr(xmat)
@@ -102,56 +144,44 @@ get_ve_t <- function(n, data, indices) {
 # n_days_period <- 7
 # n_period <- round(n_days/n_days_period)
 results <- boot(data=mi_data_iiv, statistic=get_ve_t,
-                R=1000, n = 40
+                R=1000, n = 63
                 # n_days_period = n_days_period,
-                # n_days = n_days,n_period = n_period
+                # n_days = n_days, n_period = n_period
                 )
 
 # view results
-results
+#results
 plot(results)
 
 # get 95% confidence interval
-boot.ci(results, index = 1)
-
-
-
-# ---------------------------------------------------------------------------------------
-# output (save for each vaccine)
-# IIV
+# boot.ci(results, index = 1)
 first_date <- mi_data_iiv[which(mi_data_iiv$DINF_new == min(time_points)),]$onset_date
-pred_dates <- first_date + (time_points - min(time_points))
-ve_dat_iiv <- tibble(time_point = time_points,
-                 date = pred_dates,
-                 ve = ve_t,
-                 lower = ve_t_upper,
-                 upper = ve_t_lower,
-                 vac_type = "IIV")
+pred_dates <- seq.Date(from = first_date, to = first_date + (max(time_points) - min(time_points)), length.out = 63)
 
-# LAIV
-first_date <- mi_data_laiv[which(mi_data_laiv$DINF_new == min(time_points)),]$onset_date
-pred_dates <- first_date + (time_points - min(time_points))
-ve_dat_laiv <- tibble(time_point = time_points,
-                     date = pred_dates,
-                     ve = ve_t,
-                     lower = ve_t_upper,
-                     upper = ve_t_lower,
-                     vac_type = "LAIV")
-# for output
-ve_output <- bind_rows(ve_dat_iiv, ve_dat_laiv)
-write_csv(ve_output, file = "VE_est_durham.csv")
-# combine data sets for plotting and output
-ve_dat_plot <- ve_output %>%
-  mutate(lower = ifelse(lower < -0.25, -0.25, lower))
+boot_dat <- data.frame(date = pred_dates,
+                       V = results$t0,
+                       lower = apply(results$t, 2, quantile, probs = 0.025),
+                       upper = apply(results$t, 2, quantile, probs = 0.975)
+                       )
+fit <- lm(V ~ date, boot_dat)
 
-p_durham <- ggplot(data = ve_dat_plot, aes(x = date, y = ve)) +
-  geom_line()+
-  geom_ribbon(aes(ymin = lower, ymax = upper), alpha=0.2) +
-  labs(y = "VE(t)", x = "Date") +
-  scale_y_continuous(limits = c(-0.25, 1)) +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  theme_bw() +
-  facet_grid(. ~ vac_type)
+# plots
+plot(V ~ date, data = boot_dat)
+abline(fit)
 
-ggsave("VE_est_plot_durham1.png", p_durham)
+p_durham <- ggplot(data = boot_dat, aes(x = date, y = V)) +
+  geom_line() +
+  geom_ribbon(aes(ymin = lower, ymax = upper), alpha=0.1) +
+  labs(y = "VE(t)", x = "Date") + # , title = "Natural Spline"
+  #scale_y_continuous(limits = c(-5, 1)) +
+  #geom_hline(yintercept = 0, linetype = "dashed") +
+  #facet_grid(. ~ vac_type) +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line(colour = "black"))
+p_durham
 
+ggsave(filename = "inst/extdata/output/figure2.jpg", plot = p_durham)
+p_both <- plot_grid(p_durham, p_petrie)
+p_both
