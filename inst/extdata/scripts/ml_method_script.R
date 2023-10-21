17964d20bd3695731add4bd387644df041a056bd
Kylie Ainslie <ainslie.kylie@gmail.com>
2023-10-21 11:15:43 +0200

    initialise script for using the ML method to estimate rate of waning from simulated and RCT data

---
+++ inst/extdata/scripts/ml_method_script.R
@@ -0,0 +1,5 @@
+### ML method script ###
+# ------------------------------------------------------------------------------
+# We will use the ML method to estimate the rate of waning, first from simulated
+# data and then from RCT data from the Michigan trial.
+# ------------------------------------------------------------------------------
